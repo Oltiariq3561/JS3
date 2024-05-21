4556dd6f9f88b4af63ecd44e78a61d634a29120d
Oltiariq3561 <oltiariq356@gmail.com>
2024-05-21 14:41:06 -0700

    tugadi

---
+++ index.js
@@ -0,0 +1,225 @@
+// 1- masala
+
+// let a = + prompt ("son");
+
+// if (a>0){
+// console.log(true);
+// }
+
+
+// 2-masala
+
+// let a = + prompt ("son");
+
+// if(a % 2 ==1){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 3-masala
+
+// let a = + prompt ("son");
+
+// if(a % 2 ==0){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 4-masala
+
+// let a = + prompt ("son");
+
+// let b = + prompt ('son');
+
+// if (a>2 && b<=3) {
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 5-masala
+
+// let a = + prompt ("son");
+
+// let b = + prompt ('son');
+
+// if (a>=0 || b<-2) {
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 6-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if(a<=b<=c){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 7-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if(b>a && b<c || b<a && b>c){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 8-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// if(a % 2==1 || b % 2 == 1){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 9-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// if(a % 2==1 && b % 2 == 0 || a % 2==0 && b % 2 == 1){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 10-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 11-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// if ((a % 2 == 0 && b % 2 == 0) || (a % 2 == 1 && b % 2 == 1)){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 12-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if (a>0 && b>0 && c>0){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 13-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if (a>0 || b>0 || c>0){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 14-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if ((a>0 && b<0 && c<0) || (a<0 && b>0 && c<0) || (a<0 && b<0 && c>0)){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 15-masala
+
+// let a = + prompt ('son');
+
+// let b = + prompt ('son');
+
+// let c = + prompt ('son');
+
+// if ((a>0 && b>0 && c<0) || (a<0 && b>0 && c>0) || (a>0 && b<0 && c>0)){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+// 16-masala
+
+// let a = + prompt ('son');
+
+// if (a > 9 && a<100 && a % 2 == 0){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+
+
+// QOSHIMCHA
+
+// 17-Masala
+
+// let a = + prompt ('son');
+
+// if (a > 99 && a<1000 && a % 2 == 1){
+// console.log(true);
+// }else{
+// console.log(false);
+// }
+
+// DOMLA MENIMCHA QOLGAN MISOLLARNI MAVZUDA OTMAGANMIZ
+
